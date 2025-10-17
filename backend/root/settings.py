60e81f007c60d3f6c70f31f0c5b16f006867805c
Santosh Rawal <awesomekid.spa@gmail.com>
2025-10-17 23:27:17 +0545

    added static

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -144,3 +144,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
